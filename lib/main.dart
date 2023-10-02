7c02c3cf4cafb8b4799eb8295f352f28e64286d9
Tahmid-kabir-addin <tahmidkabiraddin@gmail.com>
2023-10-03 01:43:57 +0600

    Card Drawer StateFulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,34 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var myText = "Your Name";
+  TextEditingController _myTextController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.blue,
         title: const Text(
           'My Apps',
           style: TextStyle(
@@ -26,64 +46,97 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(
-        // height: 1000,
-        width: 500,
-        color: Colors.green,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.only(bottom: 10),
-              height: 50,
-              width: 100,
-              // color: Colors.red,
-              decoration: const BoxDecoration(
-                  color: Colors.blue,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 50,
-                    )
-                  ]
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 10),
-              height: 50,
-              width: 100,
-              // color: Colors.yellow,
-              decoration: const BoxDecoration(
-                  color: Colors.yellow,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 50,
-                    )
-                  ]
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 10),
-              height: 50,
-              width: 100,
-              // color: Colors.blue,
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 50,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Card(
+                color: Colors.grey[200],
+                child: Column(children: [
+                  Image.asset(
+                    'assets/coding.jpg',
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    myText,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(50.0),
+                    child: TextField(
+                      controller: _myTextController,
+                      decoration: InputDecoration(
+                          hintText: "Enter Name",
+                          labelText: "Name",
+                          border: OutlineInputBorder()),
+                    ),
                   )
-                ]
+                ])),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: [
+            const UserAccountsDrawerHeader(
+              accountName: Text("Tahmid Kabir"),
+              accountEmail: Text("tahmidkabiraddin@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/313269540_3155884901390464_7202136195450832068_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=1b51e3&_nc_eui2=AeEhV_AnFFQc-YNcTY2UuMIWlnRB7l2hr5OWdEHuXaGvk-p7f4fZOtQQT9yOvgF4DdY6wbTOkGGvCU61qMIaUuip&_nc_ohc=rGZ_Kb4pwWgAX-m6EDS&_nc_ht=scontent.fdac31-1.fna&oh=00_AfCbLUueO8RrHgQ-puovY6FOWFciiPH99quEfAeV4zbfBA&oe=651FAEE5"),
               ),
+            ),
+            // const DrawerHeader(
+            //   decoration: BoxDecoration(color: Colors.greenAccent),
+            //   child: Text('Tahmid Kabir'),
+            //
+            // ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Tahmid Kabir Addin'),
+              subtitle: const Text(
+                'tahmidkabiraddin@gmail.com',
+                style: TextStyle(fontSize: 12),
+              ),
+              trailing: Icon(Icons.edit),
+              onTap: () {},
+            ),
+            const ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Tahmid Kabir Addin'),
+              subtitle: Text(
+                'tahmidkabiraddin@gmail.com',
+                style: TextStyle(fontSize: 12),
+              ),
+              trailing: Icon(Icons.edit),
+            ),
+            const ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Tahmid Kabir Addin'),
+              subtitle: Text(
+                'tahmidkabiraddin@gmail.com',
+                style: TextStyle(fontSize: 12),
+              ),
+              trailing: Icon(Icons.edit),
             ),
           ],
         ),
-
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          myText = _myTextController.text;
+          setState(() {});
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.send, color: Colors.white),
+      ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
